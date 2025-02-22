ba214059a631fcd8902e5c5f2e15b64ef8bdd19b
trueddn-me <mail@farukaltun.com.tr>
2025-02-22 23:08:53 +0300

    SMTP Test Tool Genel Hatları ile oluşturuldu. Düzenleme yapılması gerekecek ama

--- TrueTestingTool/Form1.Designer.cs
+++ TrueTestingTool/Form1.Designer.cs
@@ -31,6 +31,35 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            groupBox2 = new GroupBox();
+            smtplog = new RichTextBox();
+            groupBox1 = new GroupBox();
+            outauthporttxt = new TextBox();
+            inauthporttxt = new TextBox();
+            label11 = new Label();
+            label10 = new Label();
+            messatestbutton = new Button();
+            label9 = new Label();
+            noneradio = new RadioButton();
+            tlsradio = new RadioButton();
+            sslradio = new RadioButton();
+            label8 = new Label();
+            outmailtxt = new TextBox();
+            label7 = new Label();
+            inmailtxt = new TextBox();
+            pwusertxt = new TextBox();
+            label6 = new Label();
+            label5 = new Label();
+            mailusertxt = new TextBox();
+            label4 = new Label();
+            bodyrichtext = new RichTextBox();
+            label3 = new Label();
+            subjecttext = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            tomailtxt = new TextBox();
+            formmailtext = new TextBox();
+            mailtestlabelsonuc = new Label();
             mailaction1 = new Button();
             radioping = new RadioButton();
             radioporttesting = new RadioButton();
@@ -39,9 +68,10 @@
             porttextbox = new TextBox();
             mserverbox = new TextBox();
             tabPage2 = new TabPage();
-            mailtestlabelsonuc = new Label();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // tabControl1
@@ -51,11 +81,13 @@
             tabControl1.Location = new Point(12, 32);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(776, 406);
+            tabControl1.Size = new Size(1066, 676);
             tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(groupBox2);
+            tabPage1.Controls.Add(groupBox1);
             tabPage1.Controls.Add(mailtestlabelsonuc);
             tabPage1.Controls.Add(mailaction1);
             tabPage1.Controls.Add(radioping);
@@ -68,10 +100,288 @@
             tabPage1.Location = new Point(4, 24);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(768, 378);
+            tabPage1.Size = new Size(1058, 648);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Smtp Test Tool";
             tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(smtplog);
+            groupBox2.Location = new Point(7, 488);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(1045, 154);
+            groupBox2.TabIndex = 11;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Log";
+            // 
+            // smtplog
+            // 
+            smtplog.Location = new Point(31, 22);
+            smtplog.Name = "smtplog";
+            smtplog.Size = new Size(993, 117);
+            smtplog.TabIndex = 10;
+            smtplog.Text = "";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(outauthporttxt);
+            groupBox1.Controls.Add(inauthporttxt);
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(messatestbutton);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(noneradio);
+            groupBox1.Controls.Add(tlsradio);
+            groupBox1.Controls.Add(sslradio);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(outmailtxt);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(inmailtxt);
+            groupBox1.Controls.Add(pwusertxt);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(mailusertxt);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(bodyrichtext);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(subjecttext);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(tomailtxt);
+            groupBox1.Controls.Add(formmailtext);
+            groupBox1.Location = new Point(7, 65);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(704, 381);
+            groupBox1.TabIndex = 8;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Message Test";
+            // 
+            // outauthporttxt
+            // 
+            outauthporttxt.Location = new Point(615, 138);
+            outauthporttxt.Name = "outauthporttxt";
+            outauthporttxt.Size = new Size(48, 23);
+            outauthporttxt.TabIndex = 35;
+            // 
+            // inauthporttxt
+            // 
+            inauthporttxt.Location = new Point(615, 109);
+            inauthporttxt.Name = "inauthporttxt";
+            inauthporttxt.Size = new Size(48, 23);
+            inauthporttxt.TabIndex = 34;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(575, 141);
+            label11.Name = "label11";
+            label11.Size = new Size(38, 15);
+            label11.TabIndex = 33;
+            label11.Text = "Port : ";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(575, 116);
+            label10.Name = "label10";
+            label10.Size = new Size(38, 15);
+            label10.TabIndex = 32;
+            label10.Text = "Port : ";
+            // 
+            // messatestbutton
+            // 
+            messatestbutton.Location = new Point(454, 192);
+            messatestbutton.Name = "messatestbutton";
+            messatestbutton.Size = new Size(201, 23);
+            messatestbutton.TabIndex = 12;
+            messatestbutton.Text = "Başlat";
+            messatestbutton.UseVisualStyleBackColor = true;
+            messatestbutton.Click += messatestbutton_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label9.Location = new Point(453, 25);
+            label9.Name = "label9";
+            label9.Size = new Size(202, 25);
+            label9.TabIndex = 31;
+            label9.Text = "Mail Kullanıcı Bilgileri";
+            // 
+            // noneradio
+            // 
+            noneradio.AutoSize = true;
+            noneradio.Location = new Point(601, 167);
+            noneradio.Name = "noneradio";
+            noneradio.Size = new Size(54, 19);
+            noneradio.TabIndex = 30;
+            noneradio.TabStop = true;
+            noneradio.Text = "None";
+            noneradio.UseVisualStyleBackColor = true;
+            // 
+            // tlsradio
+            // 
+            tlsradio.AutoSize = true;
+            tlsradio.Location = new Point(527, 167);
+            tlsradio.Name = "tlsradio";
+            tlsradio.Size = new Size(68, 19);
+            tlsradio.TabIndex = 29;
+            tlsradio.TabStop = true;
+            tlsradio.Text = "StartTLS";
+            tlsradio.UseVisualStyleBackColor = true;
+            // 
+            // sslradio
+            // 
+            sslradio.AutoSize = true;
+            sslradio.Location = new Point(454, 167);
+            sslradio.Name = "sslradio";
+            sslradio.Size = new Size(67, 19);
+            sslradio.TabIndex = 28;
+            sslradio.TabStop = true;
+            sslradio.Text = "SSL/TLS";
+            sslradio.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(359, 141);
+            label8.Name = "label8";
+            label8.Size = new Size(87, 15);
+            label8.TabIndex = 27;
+            label8.Text = "Giden Sunucu :";
+            // 
+            // outmailtxt
+            // 
+            outmailtxt.Location = new Point(454, 138);
+            outmailtxt.Name = "outmailtxt";
+            outmailtxt.Size = new Size(115, 23);
+            outmailtxt.TabIndex = 26;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(360, 113);
+            label7.Name = "label7";
+            label7.Size = new Size(86, 15);
+            label7.TabIndex = 25;
+            label7.Text = "Gelen Sunucu :";
+            // 
+            // inmailtxt
+            // 
+            inmailtxt.Location = new Point(454, 110);
+            inmailtxt.Name = "inmailtxt";
+            inmailtxt.Size = new Size(115, 23);
+            inmailtxt.TabIndex = 24;
+            // 
+            // pwusertxt
+            // 
+            pwusertxt.Location = new Point(454, 81);
+            pwusertxt.Name = "pwusertxt";
+            pwusertxt.Size = new Size(209, 23);
+            pwusertxt.TabIndex = 23;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(400, 85);
+            label6.Name = "label6";
+            label6.Size = new Size(46, 15);
+            label6.TabIndex = 22;
+            label6.Text = "Parola :";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(366, 61);
+            label5.Name = "label5";
+            label5.Size = new Size(82, 15);
+            label5.TabIndex = 21;
+            label5.Text = "Kullanıcı Adı : ";
+            // 
+            // mailusertxt
+            // 
+            mailusertxt.Location = new Point(453, 53);
+            mailusertxt.Name = "mailusertxt";
+            mailusertxt.Size = new Size(210, 23);
+            mailusertxt.TabIndex = 20;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(17, 156);
+            label4.Name = "label4";
+            label4.Size = new Size(40, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Body :";
+            // 
+            // bodyrichtext
+            // 
+            bodyrichtext.Location = new Point(72, 153);
+            bodyrichtext.Name = "bodyrichtext";
+            bodyrichtext.Size = new Size(279, 136);
+            bodyrichtext.TabIndex = 6;
+            bodyrichtext.Text = "Mesaj İçeriğini Buraya Yazınız";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(5, 116);
+            label3.Name = "label3";
+            label3.Size = new Size(52, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Subject :";
+            // 
+            // subjecttext
+            // 
+            subjecttext.Location = new Point(72, 113);
+            subjecttext.Name = "subjecttext";
+            subjecttext.Size = new Size(279, 23);
+            subjecttext.TabIndex = 4;
+            subjecttext.Text = "Mail Başlığını Buraya Yazınız";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(31, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(26, 15);
+            label2.TabIndex = 3;
+            label2.Text = "To :";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(21, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(41, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Form :";
+            // 
+            // tomailtxt
+            // 
+            tomailtxt.Location = new Point(72, 68);
+            tomailtxt.Name = "tomailtxt";
+            tomailtxt.Size = new Size(157, 23);
+            tomailtxt.TabIndex = 1;
+            tomailtxt.Text = "mail2@mail2.com";
+            // 
+            // formmailtext
+            // 
+            formmailtext.Location = new Point(72, 38);
+            formmailtext.Name = "formmailtext";
+            formmailtext.Size = new Size(157, 23);
+            formmailtext.TabIndex = 0;
+            formmailtext.Text = "mail@mail.com";
+            // 
+            // mailtestlabelsonuc
+            // 
+            mailtestlabelsonuc.AutoSize = true;
+            mailtestlabelsonuc.Location = new Point(489, 29);
+            mailtestlabelsonuc.Name = "mailtestlabelsonuc";
+            mailtestlabelsonuc.Size = new Size(37, 15);
+            mailtestlabelsonuc.TabIndex = 7;
+            mailtestlabelsonuc.Text = "..........";
             // 
             // mailaction1
             // 
@@ -142,25 +452,16 @@
             tabPage2.Location = new Point(4, 24);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(768, 378);
+            tabPage2.Size = new Size(1058, 648);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "tabPage2";
             tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // mailtestlabelsonuc
-            // 
-            mailtestlabelsonuc.AutoSize = true;
-            mailtestlabelsonuc.Location = new Point(597, 28);
-            mailtestlabelsonuc.Name = "mailtestlabelsonuc";
-            mailtestlabelsonuc.Size = new Size(37, 15);
-            mailtestlabelsonuc.TabIndex = 7;
-            mailtestlabelsonuc.Text = "..........";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1090, 720);
             Controls.Add(tabControl1);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
@@ -169,6 +470,9 @@
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
             tabPage1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -185,5 +489,33 @@
         private RadioButton radioping;
         private RadioButton radioporttesting;
         private Label mailtestlabelsonuc;
+        private GroupBox groupBox1;
+        private RichTextBox bodyrichtext;
+        private Label label3;
+        private TextBox subjecttext;
+        private Label label2;
+        private Label label1;
+        private TextBox tomailtxt;
+        private TextBox formmailtext;
+        private GroupBox groupBox2;
+        private RichTextBox smtplog;
+        private Label label4;
+        private Label label9;
+        private RadioButton noneradio;
+        private RadioButton tlsradio;
+        private RadioButton sslradio;
+        private Label label8;
+        private TextBox outmailtxt;
+        private Label label7;
+        private TextBox inmailtxt;
+        private TextBox pwusertxt;
+        private Label label6;
+        private Label label5;
+        private TextBox mailusertxt;
+        private Button messatestbutton;
+        private TextBox outauthporttxt;
+        private TextBox inauthporttxt;
+        private Label label11;
+        private Label label10;
     }
 }
--- TrueTestingTool/Form1.cs
+++ TrueTestingTool/Form1.cs
@@ -1,4 +1,9 @@
-using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
 
 namespace TrueTestingTool
 {
@@ -7,6 +12,9 @@ namespace TrueTestingTool
         public Form1()
         {
             InitializeComponent();
+            // Label'ý pasif yap (görünmez)
+            mailtestlabelsonuc.Visible = false;
+
             // Formun tam ekran butonunu devre dýþý býrakma
             this.MaximizeBox = false; // Tam ekran butonunu devre dýþý býrak
 
@@ -24,20 +32,22 @@ namespace TrueTestingTool
 
             // KeyPress olayýný baðlama
             porttextbox.KeyPress += new KeyPressEventHandler(porttextbox_KeyPress);
+            inauthporttxt.KeyPress += new KeyPressEventHandler(porttextbox_KeyPress);
+            outauthporttxt.KeyPress += new KeyPressEventHandler(porttextbox_KeyPress);
 
             // Form yüklendiðinde butonun durumunu ayarla
             UpdateButtonState();
             // RadioButton'larýn CheckedChanged olayýný baðlama
             radioporttesting.CheckedChanged += RadioButton_CheckedChanged;
             radioping.CheckedChanged += RadioButton_CheckedChanged;
+            smtplog.ReadOnly = true;
 
-
+            sslradio.CheckedChanged += RadioButton_CheckedChanged;
+            tlsradio.CheckedChanged += RadioButton_CheckedChanged;
+            noneradio.CheckedChanged += RadioButton_CheckedChanged;
 
 
         }
-
-
-
 
         private void porttextbox_KeyPress(object? sender, KeyPressEventArgs e)
         {
@@ -48,25 +58,185 @@ namespace TrueTestingTool
             }
         }
 
+        private void inauthporttxt_KeyPress(object? sender, KeyPressEventArgs e)
+        {
+            // Sadece sayýlar ve kontrol tuþlarýna izin ver (örneðin, Backspace)
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Karakteri engelle
+            }
+        }
+
+        private void outauthporttxt_KeyPress(object? sender, KeyPressEventArgs e)
+        {
+            // Sadece sayýlar ve kontrol tuþlarýna izin ver (örneðin, Backspace)
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Karakteri engelle
+            }
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            // Form yüklendiðinde yapýlacak iþlemler
         }
 
         private void mailaction1_Click(object sender, EventArgs e)
         {
+            // Sadece radioporttesting seçili ise iþlem yap
+            if (radioporttesting.Checked)
+            {
+                string server = mserverbox.Text;
+                int port;
 
+                // Port bilgisini sayýya çevirme
+                if (int.TryParse(porttextbox.Text, out port))
+                {
+                    // Telnet iþlemi (10 saniye timeout ile)
+                    bool isPortOpen = CheckPort(server, port, 10000); // 10 saniye = 10000 milisaniye
+
+                    // Sonucu label'a yazma
+                    mailtestlabelsonuc.Visible = true;
+                    mailtestlabelsonuc.Text = isPortOpen ? "Port Testi Baþarýlý" : "Port Eriþime Kapalý";
+                }
+                else
+                {
+                    mailtestlabelsonuc.Visible = true;
+                    mailtestlabelsonuc.Text = "Geçersiz Port Numarasý";
+                }
+            }
+            else if (radioping.Checked)
+            {
+                // Ping iþlemi yap (10 saniye timeout ile)
+                string server = mserverbox.Text;
+                bool isPingSuccessful = PingServer(server, 10000); // 10 saniye = 10000 milisaniye
+
+                // Sonucu label'a yazma
+                mailtestlabelsonuc.Visible = true;
+                mailtestlabelsonuc.Text = isPingSuccessful ? "Ping Baþarýlý" : "Ping Baþarýsýz";
+            }
         }
-        private void RadioButton_CheckedChanged(object sender, EventArgs e)
+
+        private void RadioButton_CheckedChanged(object? sender, EventArgs e)
         {
             // Butonun durumunu güncelle
             UpdateButtonState();
         }
 
+
+
         private void UpdateButtonState()
         {
             // Herhangi bir RadioButton seçili ise butonu aktif et
             mailaction1.Enabled = radioporttesting.Checked || radioping.Checked;
+            messatestbutton.Enabled = AreAllFieldsFilled() && (sslradio.Checked || tlsradio.Checked || noneradio.Checked);
+        }
+
+
+        // Tüm bilgilerin girilip girilmediðini kontrol eden metod
+        private bool AreAllFieldsFilled()
+        {
+            return !string.IsNullOrEmpty(mailusertxt.Text) &&
+                   !string.IsNullOrEmpty(pwusertxt.Text) &&
+                   !string.IsNullOrEmpty(inmailtxt.Text) &&
+                   !string.IsNullOrEmpty(outmailtxt.Text) &&
+                   !string.IsNullOrEmpty(inauthporttxt.Text) &&
+                   !string.IsNullOrEmpty(outauthporttxt.Text) &&
+                   !string.IsNullOrEmpty(formmailtext.Text) &&
+                   !string.IsNullOrEmpty(tomailtxt.Text) &&
+                   !string.IsNullOrEmpty(subjecttext.Text) &&
+                   !string.IsNullOrEmpty(bodyrichtext.Text);
+        }
+        // Telnet iþlemi için fonksiyon (timeout eklenmiþ)
+        private bool CheckPort(string server, int port, int timeout)
+        {
+            try
+            {
+                using (TcpClient client = new TcpClient())
+                {
+                    // Belirtilen sunucu ve porta baðlanmayý dene (timeout ile)
+                    var result = client.BeginConnect(server, port, null, null);
+                    var success = result.AsyncWaitHandle.WaitOne(timeout); // Timeout süresi
+                    if (success)
+                    {
+                        client.EndConnect(result);
+                        return true; // Baðlantý baþarýlý
+                    }
+                    else
+                    {
+                        client.Close();
+                        return false; // Timeout: Baðlantý baþarýsýz
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false; // Baðlantý baþarýsýz
+            }
+        }
+
+        // Ping iþlemi için fonksiyon (timeout eklenmiþ)
+        private bool PingServer(string server, int timeout)
+        {
+            try
+            {
+                using (Ping ping = new Ping())
+                {
+                    // Ping iþlemini timeout ile gerçekleþtir
+                    PingReply reply = ping.Send(server, timeout);
+                    return reply.Status == IPStatus.Success;
+                }
+            }
+            catch (Exception)
+            {
+                return false; // Ping baþarýsýz
+            }
+        }
+
+        private void messatestbutton_Click(object sender, EventArgs e)
+        {
+            if (!AreAllFieldsFilled())
+            {
+                MessageBox.Show("Lütfen tüm bilgileri doldurun.", "Uyarý", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                // SMTP istemcisini oluþtur
+                SmtpClient smtpClient = new SmtpClient(outmailtxt.Text, int.Parse(outauthporttxt.Text))
+                {
+                    EnableSsl = sslradio.Checked,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(mailusertxt.Text, pwusertxt.Text)
+                };
+
+                // Mail mesajýný oluþtur
+                MailMessage mailMessage = new MailMessage
+                {
+                    From = new MailAddress(formmailtext.Text),
+                    Subject = subjecttext.Text,
+                    Body = bodyrichtext.Text,
+                    IsBodyHtml = true
+                };
+                mailMessage.To.Add(tomailtxt.Text);
+
+                // Log ekle
+                smtplog.AppendText($"{DateTime.Now}: SMTP istemcisi oluþturuldu.\n");
+
+                // Mail gönderme iþlemi
+                smtpClient.Send(mailMessage);
+
+                // Log ekle
+                smtplog.AppendText($"{DateTime.Now}: Mail baþarýyla gönderildi.\n");
+            }
+            catch (Exception ex)
+            {
+                // Hata logu ekle
+                smtplog.AppendText($"{DateTime.Now}: Hata - {ex.Message}\n");
+                MessageBox.Show($"Mail gönderilirken bir hata oluþtu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
     }
 }
